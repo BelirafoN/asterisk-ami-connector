5be4ba19ad3e0b898dcc721332aa76181977baba
Alex Voronyansky <belirafon@gmail.com>
2016-04-26 14:01:24 +0300

    added src files

---
+++ examples/baseConnection.js
@@ -0,0 +1,19 @@
+/**
+ * Developer: Alex Voronyansky <belirafon@gmail.com>
+ * Date: 26.04.2016
+ * Time: 13:47
+ */
+
+"use strict";
+
+const connector = require('asterisk-ami-connection')({});
+
+connector.connect('login', 'password', {host: '127.0.0.1', port: 5038})
+    .then(amiConnection => {
+        amiConnection
+            .on('event', event  => console.log(event))
+            .on('response', response  => console.log(response))
+            .on('close', ()  => console.log('closed'))
+            .on('error', error  => console.log(error));
+    })
+    .catch(error => console.log(error));
